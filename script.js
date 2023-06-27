8d87407d470636a2df1003c0fea8eb0e98ddbc9e
samueljustus <113977877+samueljustus@users.noreply.github.com>
2023-06-27 08:48:24 +0100

    updated

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
     let form = document.querySelector('.form')
-    let d1 = document.querySelector('#day').value
-    let m1 = document.querySelector('#month').value
-    let y1 = document.querySelector('#year').value
+    let day = document.querySelector('#day')
+    let month= document.querySelector('#month')
+    let year = document.querySelector('#year')
+
+    let d1 = day.value
+    let m1 = month.value
+    let y1 = year.value
 
     let today = new Date();
+    
     let d2 = today.getDate();
     let m2 = today.getMonth() + 1
     let y2 = today.getFullYear();
-    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     let d, y, m
 
-    form.addEventListener('submit', checkInput)
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+    })
 
-    function checkInput(e) {
-        if (d1 === "") {
-            e.peventDefault
-            errorMsg(d1, 'this field cannot be empty')
-        }
-        else {
-        }
-    }
-    
-    function errorMsg(input, message) {
-        const parentDiv = document.querySelectorAll('input').parent
-        const p = parentDiv.querySelector('p')
-        parentDiv.classList.add('error')
-        p.innerText = message
-    }
-    
-    
+
+
 
     // function calculateAge() {
     //     
     //     if (d1 > d2) {
-    //         d2 = d2 + month[m2 - 1]
+    //         d2 = d2 + months[m2 - 1]
     //         m2--
     //     }  
     //     if (m1 > m2) {
@@ -44,8 +36,5 @@
     //        d = d2 - d1;
     //        y = y2 - y1;
     //        m = m2 - m1
-        
-    //     const age = document.querySelector('.result')
-    //     age.innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
     // }
 
