17d88cfbd0b073a7c9b870a5511643da8359957d
samueljustus <113977877+samueljustus@users.noreply.github.com>
2023-07-01 13:40:02 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-// const dayJsObj = dayjs()
-// console.log(dayJsObj.format("DD/MM/YYYY"))
 const form = document.querySelector('.form')
-const day = document.querySelector('#day')
-let dayValue = day.value;
-const month = document.querySelector('#month')
-let monthValue = month.value
-const year = document.querySelector('#year')
-let yearValue = year.value;
+const dayInput = document.querySelector('#day')
+const monthInput = document.querySelector('#month')
+const yearInput = document.querySelector('#year')
+const input = document.querySelectorAll('input')
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    validateDate()
-})
+
+const today = new Date();
+let day = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+
+function validateDate() {
+    let validator = true;  // validator is initially set to true
+    input.forEach((input) => {
+        if(input.value === "") {
+            const parent = input.parentElement
+            const p = parent.querySelector('p')
+            parent.classList.add('error')
+            p.innerText = 'this field is required'
+            validator = false;   // validator is now false because input is an empty field
+        }
+        else if (monthInput.value > 12) {
+            const parent = monthInput.parentElement
+            const p = parent.querySelector('p')
+            parent.classList.add('error')
+            p.innerText = 'Must be a valid month'
+            validator = false;
+        }
+        else if (dayInput.value > 31) {
+            const parent = dayInput.parentElement
+            const p = parent.querySelector('p')
+            parent.classList.add('error')
+            p.innerText = 'Must be a valid day'
+            validator = false;
+        }
+        else {
+            const parent = input.parentElement
+            parent.classList.remove('error')
+            const p = parent.querySelector('p')
+            p.innerText = ''
+            validator = true;
+        }
+        
+    })
+    return validator
+}
